e35d22d82f45507de5deb4cacfff82edff80da9c
Developer <dev@project-cleanroom.com>
2026-08-20 13:09:38 +0530

    Deploy all remaining uncommitted templates and UI files [skip ci]

--- test_country_glance.py
+++ test_country_glance.py
@@ -12,6 +12,7 @@ class TestCountryGlanceReport(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
+        app.config['LOGIN_DISABLED'] = True
         self.client = app.test_client()
         _build_glance_data()
 
